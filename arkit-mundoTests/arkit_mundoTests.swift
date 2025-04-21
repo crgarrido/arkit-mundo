4a84e9e8cf28d4a0fa79d94e740d568bb36b009a
Cristian Garry Garrido <cristiangarridojana@gmail.com>
2025-04-21 09:49:43 -0400

    Initial Commit

---
+++ arkit-mundoTests/arkit_mundoTests.swift
@@ -0,0 +1,17 @@
+//
+//  arkit_mundoTests.swift
+//  arkit-mundoTests
+//
+//  Created by Cristian Garrido Jaña on 21-04-25.
+//
+
+import Testing
+@testable import arkit_mundo
+
+struct arkit_mundoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ arkit-mundoUITests/arkit_mundoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  arkit_mundoUITestsLaunchTests.swift
+//  arkit-mundoUITests
+//
+//  Created by Cristian Garrido Jaña on 21-04-25.
+//
+
+import XCTest
+
+final class arkit_mundoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
